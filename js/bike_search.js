76866078055c345cbbb17dd0b037d6d260fbf133
Brendan Hanna and jordan mysliwiec <jordan@>
2017-06-29 17:02:40 -0700

    comment map out of index and add video background header

--- js/bike_search.js
+++ js/bike_search.js
@@ -8,7 +8,6 @@ BikeSearch.prototype.locateBike = function(make, location, distance, displayBike
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=' + make + '&location=' + location + '&distance=' + distance + '&stolenness=proximity&access_token=' + apiKey).then(function(response) {
     //call displayBikes which appends properties of the api response into our html
     displayBikes(response.bikes);
-    alert('hi');
   }).fail(function(error) {
     $('.showBikes').text(error.responseJSON.message);
   });
